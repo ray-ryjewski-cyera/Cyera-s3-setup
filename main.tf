bbaa90364bee815c9f623a40b8fab1954a955e9c
ray-ryjewski-cyera <ray.ryjewski@cyera.io>
2023-12-04 17:25:38 -0500

    Default to OSS

--- main.tf
+++ main.tf
@@ -1,20 +1,16 @@
 terraform {
-  cloud {
-    organization = "brightblueray"
+  # cloud {
+  #   organization = "brightblueray"  #Update to your TFC Org 
 
-    workspaces {
-      name = "Cyera-lab"
-    }
-  }
+  #   workspaces {
+  #     name = "Cyera-lab"
+  #   }
+  # }
   required_providers {
     aws = {
       source = "hashicorp/aws"
       version = "5.27.0"
     }
-    # http = {
-    #   source = "hashicorp/http"
-    #   version = "3.4.0"
-    # }
   }
 }
 
@@ -25,6 +21,7 @@ provider "aws" {
   # Creds stored in TFC Workspace as ENV Variables
     #  AWS_ACCESS_KEY_ID="anaccesskey"
     #  AWS_SECRET_ACCESS_KEY="asecretkey"
+    # AWS_SESSION_TOKEN="aSessionToken"
 }
 
 # data "http" "example" {
@@ -36,15 +33,15 @@ resource "aws_s3_bucket" "private_bucket" {
     # acl = "private"
 }
 
-resource "aws_s3_bucket" "public_read_bucket" {
-    bucket = "rkr-pub-r-bucket"
-    # acl = "public-read"
-}
+# resource "aws_s3_bucket" "public_read_bucket" {
+#     bucket = "rkr-pub-r-bucket"
+#     # acl = "public-read"
+# }
 
-resource "aws_s3_bucket" "public_rw_bucket" {
-    bucket = "rkr-pub-rw-bucket"
-    # acl = "public-read-write"
-}
+# resource "aws_s3_bucket" "public_rw_bucket" {
+#     bucket = "rkr-pub-rw-bucket"
+#     # acl = "public-read-write"
+# }
 
 resource "aws_s3_object" "a1mb-Test-docx" {
   bucket = aws_s3_bucket.private_bucket.bucket
@@ -65,31 +62,31 @@ resource "aws_s3_object" "a10mb-Test-docx" {
 }
 
 resource "aws_s3_object" "a10mb-Test-xlsx" {
-  bucket = aws_s3_bucket.public_read_bucket.bucket
+  bucket = aws_s3_bucket.private_bucket.bucket
   key    = "a10mb-Test-xlsx"
   source = "samples/10-MB-Test.xlsx"
 }
 
 resource "aws_s3_object" "sample-data-csv" {
-  bucket = aws_s3_bucket.public_read_bucket.bucket
+  bucket = aws_s3_bucket.private_bucket.bucket
   key    = "sample-data-csv"
   source = "samples/sample-data.csv"
 }
 
 resource "aws_s3_object" "sample-data-pdf" {
-  bucket = aws_s3_bucket.public_rw_bucket.bucket
+  bucket = aws_s3_bucket.private_bucket.bucket
   key    = "sample-data-pdf"
   source = "samples/sample-data.pdf"
 }
 
 resource "aws_s3_object" "sample-data-xls" {
-  bucket = aws_s3_bucket.public_rw_bucket.bucket
+  bucket = aws_s3_bucket.private_bucket.bucket
   key    = "sample-data-xls"
   source = "samples/sample-data.xls"
 }
 
 resource "aws_s3_object" "sample-data-xlsx" {
-  bucket = aws_s3_bucket.public_rw_bucket.bucket
+  bucket = aws_s3_bucket.private_bucket.bucket
   key    = "sample-data-xlsx"
   source = "samples/sample-data.xlsx"
 }
