059f2b27837281ce5f849d1f4204116e4bf161d5
ray-ryjewski-cyera <ray.ryjewski@cyera.io>
2023-12-05 14:16:41 -0500

    anything in sample dir

--- main.tf
+++ main.tf
@@ -24,14 +24,27 @@ provider "aws" {
     # AWS_SESSION_TOKEN="aSessionToken"
 }
 
-# data "http" "example" {
-#     url = "https://dlptest.com/sample-data.csv"
-# }
+module "sample_files" {
+  source  = "hashicorp/dir/template"
+  version = "1.0.2"
+  
+  base_dir="samples"
+}
+
 
 resource "aws_s3_bucket" "private_bucket" {
     bucket = "rkr-priviate-bucket"
     # acl = "private"
 }
+
+resource "aws_s3_object" "sample_data_files" {
+  for_each = module.sample_files.files
+
+  bucket = aws_s3_bucket.private_bucket.bucket
+  key = each.key
+  source = each.value.source_path
+}
+
 
 # resource "aws_s3_bucket" "public_read_bucket" {
 #     bucket = "rkr-pub-r-bucket"
@@ -43,50 +56,50 @@ resource "aws_s3_bucket" "private_bucket" {
 #     # acl = "public-read-write"
 # }
 
-resource "aws_s3_object" "a1mb-Test-docx" {
-  bucket = aws_s3_bucket.private_bucket.bucket
-  key    = "a1mb-Test-docx"
-  source = "samples/1-MB-Test.docx"
-}
+# resource "aws_s3_object" "a1mb-Test-docx" {
+#   bucket = aws_s3_bucket.private_bucket.bucket
+#   key    = "a1mb-Test-docx"
+#   source = "samples/1-MB-Test.docx"
+# }
 
-resource "aws_s3_object" "a1mb-Test-xlsx" {
-  bucket = aws_s3_bucket.private_bucket.bucket
-  key    = "a1mb-Test-xlsx"
-  source = "samples/1-MB-Test.xlsx"
-}
+# resource "aws_s3_object" "a1mb-Test-xlsx" {
+#   bucket = aws_s3_bucket.private_bucket.bucket
+#   key    = "a1mb-Test-xlsx"
+#   source = "samples/1-MB-Test.xlsx"
+# }
 
-resource "aws_s3_object" "a10mb-Test-docx" {
-  bucket = aws_s3_bucket.private_bucket.bucket
-  key    = "a10mb-Test-docx"
-  source = "samples/10-MB-Test.docx"
-}
+# resource "aws_s3_object" "a10mb-Test-docx" {
+#   bucket = aws_s3_bucket.private_bucket.bucket
+#   key    = "a10mb-Test-docx"
+#   source = "samples/10-MB-Test.docx"
+# }
 
-resource "aws_s3_object" "a10mb-Test-xlsx" {
-  bucket = aws_s3_bucket.private_bucket.bucket
-  key    = "a10mb-Test-xlsx"
-  source = "samples/10-MB-Test.xlsx"
-}
+# resource "aws_s3_object" "a10mb-Test-xlsx" {
+#   bucket = aws_s3_bucket.private_bucket.bucket
+#   key    = "a10mb-Test-xlsx"
+#   source = "samples/10-MB-Test.xlsx"
+# }
 
-resource "aws_s3_object" "sample-data-csv" {
-  bucket = aws_s3_bucket.private_bucket.bucket
-  key    = "sample-data-csv"
-  source = "samples/sample-data.csv"
-}
+# resource "aws_s3_object" "sample-data-csv" {
+#   bucket = aws_s3_bucket.private_bucket.bucket
+#   key    = "sample-data-csv"
+#   source = "samples/sample-data.csv"
+# }
 
-resource "aws_s3_object" "sample-data-pdf" {
-  bucket = aws_s3_bucket.private_bucket.bucket
-  key    = "sample-data-pdf"
-  source = "samples/sample-data.pdf"
-}
+# resource "aws_s3_object" "sample-data-pdf" {
+#   bucket = aws_s3_bucket.private_bucket.bucket
+#   key    = "sample-data-pdf"
+#   source = "samples/sample-data.pdf"
+# }
 
-resource "aws_s3_object" "sample-data-xls" {
-  bucket = aws_s3_bucket.private_bucket.bucket
-  key    = "sample-data-xls"
-  source = "samples/sample-data.xls"
-}
+# resource "aws_s3_object" "sample-data-xls" {
+#   bucket = aws_s3_bucket.private_bucket.bucket
+#   key    = "sample-data-xls"
+#   source = "samples/sample-data.xls"
+# }
 
-resource "aws_s3_object" "sample-data-xlsx" {
-  bucket = aws_s3_bucket.private_bucket.bucket
-  key    = "sample-data-xlsx"
-  source = "samples/sample-data.xlsx"
-}
+# resource "aws_s3_object" "sample-data-xlsx" {
+#   bucket = aws_s3_bucket.private_bucket.bucket
+#   key    = "sample-data-xlsx"
+#   source = "samples/sample-data.xlsx"
+# }
